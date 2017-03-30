0d6bc688745f71fe4494afa9c53b9053f50fa8be
stefan_xiepj <stefan_xiepj@126.com>
2017-03-30 14:09:50 +0800

    delete /**/

--- CHECK_DATA_FILES/com/asiainfo/checkdatafiles/handler/BaseHandler.java
+++ CHECK_DATA_FILES/com/asiainfo/checkdatafiles/handler/BaseHandler.java
@@ -1,43 +1,37 @@
-/*  1:   */package com.asiainfo.checkdatafiles.handler;
-/*  2:   */
-/*  3:   */import java.util.logging.Logger;
-/*  4:   */
-/* 16:   */public abstract class BaseHandler
-/* 17:   */{
-/* 18:   */  private String handerName;
-/* 19:   */  private String parameter;
-/* 20:   */  private BaseHandler successor;
-/* 21:21 */  Logger logger = Logger.getLogger(getClass().getName());
-/* 22:   */  
-/* 23:   */  public String getHanderName() {
-/* 24:24 */    return this.handerName;
-/* 25:   */  }
-/* 26:   */  
-/* 27:   */  public void setHanderName(String handerName) {
-/* 28:28 */    this.handerName = handerName;
-/* 29:   */  }
-/* 30:   */  
-/* 31:   */  public String getParameter() {
-/* 32:32 */    return this.parameter;
-/* 33:   */  }
-/* 34:   */  
-/* 35:   */  public void setParameter(String parameter) {
-/* 36:36 */    this.parameter = parameter;
-/* 37:   */  }
-/* 38:   */  
-/* 39:   */  public BaseHandler getSuccessor() {
-/* 40:40 */    return this.successor;
-/* 41:   */  }
-/* 42:   */  
-/* 43:   */  public void setSuccessor(BaseHandler successor) {
-/* 44:44 */    this.successor = successor;
-/* 45:   */  }
-/* 46:   */  
-/* 47:   */  public abstract void handerRequest(Object paramObject);
-/* 48:   */}
+package com.asiainfo.checkdatafiles.handler;
 
+import java.util.logging.Logger;
 
-/* Location:           D:\git\CHECK_DATA_FILES\bin\
- * Qualified Name:     com.asiainfo.checkdatafiles.handler.BaseHandler
- * JD-Core Version:    0.7.0-SNAPSHOT-20130630
- */
+public abstract class BaseHandler
+{
+  private String handerName;
+  private String parameter;
+  private BaseHandler successor;
+  Logger logger = Logger.getLogger(getClass().getName());
+  
+  public String getHanderName() {
+    return this.handerName;
+  }
+  
+  public void setHanderName(String handerName) {
+    this.handerName = handerName;
+  }
+  
+  public String getParameter() {
+    return this.parameter;
+  }
+  
+  public void setParameter(String parameter) {
+    this.parameter = parameter;
+  }
+  
+  public BaseHandler getSuccessor() {
+    return this.successor;
+  }
+  
+  public void setSuccessor(BaseHandler successor) {
+    this.successor = successor;
+  }
+  
+  public abstract void handerRequest(Object paramObject);
+}
